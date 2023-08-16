d44a204ef5c581641d3cdc4a594eddbf69d05c99
1214asad <asadkhan58057@gmail.com>
2023-08-16 22:36:04 +0530

    adding port and removing localhost

--- db/sql.js
+++ db/sql.js
@@ -1,10 +1,11 @@
 // Create a MySQL connection pool
 const mysql = require("mysql2");
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "127.0.0.1t",
   user: "root",
   database: "test",
   password: "asad8445569754",
+  port: 3306,
 });
 
 module.exports = pool.promise();
