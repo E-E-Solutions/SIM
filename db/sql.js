3abe1f4cb128a61bd1ee36b78f29a6042c6726b8
1214asad <asadkhan58057@gmail.com>
2023-08-16 22:45:10 +0530

    removing error from pool

--- db/sql.js
+++ db/sql.js
@@ -1,7 +1,7 @@
 // Create a MySQL connection pool
 const mysql = require("mysql2");
 const pool = mysql.createPool({
-  host: "127.0.0.1t",
+  host: "127.0.0.1",
   user: "root",
   database: "test",
   password: "asad8445569754",
